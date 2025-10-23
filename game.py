45ec2b97a0139fac9ee0df989b61fa9ff7dd5413
LRoweGore <mistergore92@gmail.com>
2025-10-23 18:20:58 +0100

    Update game.py

--- game.py
+++ game.py
@@ -72,11 +72,11 @@ elif st.session_state.scene == "crossroads":
     st.write("As you continue following the path, you approach a crossroads, do you go left or right?")
     col1, col2 = st.columns(2)
     with col1:
-    if st.button("Left", on_click=go, args=("left_path",)):
-        pass
+        if st.button("Left", on_click=go, args=("left_path",)):
+            pass
     with col2:
-    if st.button("Right", on_click=go, args=("right_path",)):
-        pass
+        if st.button("Right", on_click=go, args=("right_path",)):
+            pass
 
 # Left path branch (lake > island > chalice or path_to_house)
 elif st.session_state.scene == "left_path":
