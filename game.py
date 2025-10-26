58eb7edde33f7f9c2e9b0f3312d872b18005b979
LRoweGore <mistergore92@gmail.com>
2025-10-26 16:08:20 +0000

    Update game.py

--- game.py
+++ game.py
@@ -192,14 +192,14 @@ elif st.session_state.scene == "path_to_house":
         pass
 
 elif st.session_state.scene =="dont_enter":
-    st.write("Your initial thoughts to turn away are probably correct, but you suddenly feel a looming presence.")
+        st.write("Your initial thoughts to turn away are probably correct, but you suddenly feel a looming presence.")
         st.write ("Suddenly, there's a loud clang to your left where a rusty intercom sits, and a yellow gas spills out and fills the space around you.")
         st.write ("You breathe it in, and before you can react, you're compelled to completely change your mind about entering the house, you walk towards the house as if under a spell... reminds you of a movie")
         st.write ("You try the front door of the house, it's open. You take a few steps forward inside, and a chill wind hits you.")
         st.write("A panic overcomes you, and you snap out of the trance you were in but as you turn to leave, the front door slams shut. A voice fills the air 'The way is shut'. With no way back, you survey the area around you. You notice a door on your left, a red ruby jewel embedded in the centre of the door. The right a door with a blue sapphire jewel embedded in the centre of its door.")
         st.write("You try the front door of the house, it's open. You take a few steps forward inside, and a chill wind hits you.")
-        if st.button("Continue", on_click=go, args=("inside_house",)):
-            pass
+    if st.button("Continue", on_click=go, args=("inside_house",)):
+        pass
 
 elif st.session_state.scene == "inside_house":
     st.write("You try the front door of the house, it's open. A panic overcomes you and then the front door slams shut. You notice two doors: one with a red ruby, one with a blue sapphire.")
