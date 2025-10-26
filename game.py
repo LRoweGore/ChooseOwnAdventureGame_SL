6ecfbc6daf65f75aece0de3f4d04dd2bfe119603
LRoweGore <mistergore92@gmail.com>
2025-10-26 15:18:12 +0000

    Update game.py

--- game.py
+++ game.py
@@ -171,7 +171,7 @@ elif st.session_state.scene == "cabin_inside":
 elif st.session_state.scene =="chest_leave":
      st.write("You get a tingle down your spine which tells you not to open the chest, and you leave the cabin.")
      if st.button("Continue", on_click=go, args=("path_to_house",)):
-     pass
+         pass
     
     
             
