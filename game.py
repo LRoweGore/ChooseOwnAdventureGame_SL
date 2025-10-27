16d701d8cd9e3d7c43b17e9f5d8be8902f106a75
LRoweGore <mistergore92@gmail.com>
2025-10-27 20:16:24 +0000

    Update game.py

--- game.py
+++ game.py
@@ -217,11 +217,11 @@ elif st.session_state.scene == "inside_house":
         st.write ("You try the front door of the house, it's open. You take a few steps forward inside, and a chill wind hits you.")
         st.write ("A panic overcomes you, and you snap out of the trance you were in but as you turn to leave, the front door slams shut! You try the door but to no avail, your only choice is forward. You notice a door on your left, a red ruby jewel embedded in the centre of the door, and another door on the right with a blue sapphire jewel embedded   in the centre of its door.")
         
-     if st.button("Open the Blue Door", on_click=go, args=("blue_path",)):
-        pass
+         if st.button("Open the Blue Door", on_click=go, args=("blue_path",)):
+            pass
         
-     if st.button("Open the Red Door", on_click=go, args=("red_path",)):
-        pass
+         if st.button("Open the Red Door", on_click=go, args=("red_path",)):
+            pass
 
 # Blue path
 elif st.session_state.scene == "blue_path":
