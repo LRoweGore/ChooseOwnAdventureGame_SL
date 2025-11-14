20495512abd5638ad16ec64244e4503b2777b568
LRoweGore <mistergore92@gmail.com>
2025-11-14 16:05:23 +0000

    Update game.py

--- game.py
+++ game.py
@@ -60,7 +60,7 @@ if st.session_state.scene == "start":
     if col2.button("No"):
         st.write("Journey another day then...")
         
-    st.write("Any and all sounds used belong to their respective copyright holders, this project is purely for educational purposes. The views, opinions expressed are those of myself Lewis Rowe, and do not reflect any other business or company I have an affiliation with.")
+    st.write("*Any and all sounds used belong to their respective copyright holders, this project is purely for educational purposes. The views, opinions expressed are those of myself Lewis Rowe, and do not reflect any other business or company I have an affiliation with.*")
         
 
 # Scene: wake_up > crossroads
